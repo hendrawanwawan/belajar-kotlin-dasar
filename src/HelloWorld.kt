5a25ee320a70c6107b971389e08acb07907f958a
hendrawanwawan <hendrawanhas6k@gmail.com>
2024-03-21 10:45:34 +0800

    First commit

---
+++ src/HelloWorld.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    /*
+
+    println untuk membuat text langusng enter
+    print tanpa enter langsung dalam satu line yang sama
+
+    */
+
+    println("Hello world")
+    println("ohaoyu minasan")
+
+    print("Hllo i'm ")
+    print("wanwan")
+}
