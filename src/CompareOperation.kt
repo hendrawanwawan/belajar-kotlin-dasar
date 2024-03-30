f706dfc839d462b0d72c143cda2acec71eadc08e
hendrawanwawan <hendrawanhas6k@gmail.com>
2024-03-30 18:02:44 +0800

    menambahkan operasi matematika

---
+++ src/CompareOperation.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //operasi perbadingan = membadingkan dua buah data ,
+    //operasi ini menghasilkan nilai boolean (benar atau salah) || (true atau false)
+    val a = 100
+    val b = 200
+
+    val result: Boolean = a > b
+    val result2 = a < b
+    println(result)
+    println(result2)
+    println(100 >= 100)
+    println(100 <= 100)
+    println("wan" > "Wan")
+    println("a" < "b")
+    println("a" == "a")
+    println("a" != "b" )
+}
